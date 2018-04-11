03f0123984248ef8aec18ec4d78229b2b9c2cd47
Kengo Ohsawa <32446611+ohsawaK@users.noreply.github.com>
2018-04-12 00:36:44 +0900

    Create TouchyWindow.java

---
+++ TouchyWindow.java
@@ -0,0 +1,13 @@
+import objectdraw.*;
+import java.awt.*;
+
+public class TouchyWindow extends WindowController {
+
+    @Override public void onMousePress( Location point ) {
+        new Text("I'm Touched", 40, 50, canvas );
+    }
+
+    @Override public void onMouseRelease( Location point ) {
+        canvas.clear();
+    }
+}
