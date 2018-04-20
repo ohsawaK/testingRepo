3c6cab09c732a48c52b50e73d3d1cc7ff165b9b6
ohsawaK <osawa.kengo.63u@st.kyoto-u.ac.jp>
2018-04-20 14:50:03 +0900

    test char code(Uni)

--- TouchyWindow.java
+++ TouchyWindow.java
@@ -6,7 +6,7 @@ import java.awt.*;
 public class TouchyWindow extends WindowController {
 
     @Override public void onMousePress( Location point ) {
-        new Text("I'm Touched", 40, 50, canvas );
+        new Text("ƒeƒXƒg", 40, 50, canvas );
     }
 
     @Override public void onMouseRelease( Location point ) {
